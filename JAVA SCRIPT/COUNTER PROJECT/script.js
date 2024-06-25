20e87317d69ffcc1bc011288fbee8426e1a225e5
Harit kumar <>
2024-06-25 17:07:32 +0530

    Counter project

---
+++ JAVA SCRIPT/COUNTER PROJECT/script.js
@@ -0,0 +1,12 @@
+const countvalue = document.querySelector('#counter');
+
+const increment = () => {
+    let value = parseInt(countvalue.innerHTML)
+    value = value+1
+    countvalue.innerHTML = value;
+}
+const decrement = () => {
+    let value = parseInt(countvalue.innerHTML)
+    value = value-1
+    countvalue.innerHTML = value;
+}
