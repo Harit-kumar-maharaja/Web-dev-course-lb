ca56a3f7a0e18cabb2b672022e874b23ffcea102
Harit kumar <>
2024-07-02 15:20:24 +0530

    Testimonials

---
+++ REACT/testimonials/src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+      <div>
+        <img></img>
+      </div>
+
+      <div>
+        <p></p>
+      </div>
+      
+      <div>
+        <p></p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
